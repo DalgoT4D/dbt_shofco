9a887d9511f3a01f9e79529e9d9ad4cde8f18035
Pratiksha Rao <pratz.rao@gmail.com>
2024-11-08 14:03:46 +0530

    .

--- models/marts/org-wide/mapping.sql
+++ models/marts/org-wide/mapping.sql
@@ -9,6 +9,18 @@ SELECT
     split_part("gps_location", ' ', 1)::float AS "latitude",
     split_part("gps_location", ' ', 2)::float AS "longitude",
     split_part("gps_location", ' ', 3)::float AS "altitude",
-    split_part("gps_location", ' ', 4)::float AS "accuracy"
-    
+    split_part("gps_location", ' ', 4)::float AS "accuracy",
+
+    -- Create "Active Programs" column based on program columns
+    CASE
+        WHEN "wash_program" = 'yes' AND "gender_program" = 'yes' AND "shofco_education_program" = 'yes' THEN 'WASH, Gender, Education'
+        WHEN "wash_program" = 'yes' AND "gender_program" = 'yes' THEN 'WASH, Gender'
+        WHEN "wash_program" = 'yes' AND "shofco_education_program" = 'yes' THEN 'WASH, Education'
+        WHEN "gender_program" = 'yes' AND "shofco_education_program" = 'yes' THEN 'Gender, Education'
+        WHEN "wash_program" = 'yes' THEN 'WASH'
+        WHEN "gender_program" = 'yes' THEN 'Gender'
+        WHEN "shofco_education_program" = 'yes' THEN 'Education'
+        ELSE NULL
+    END AS "Active Programs"
+
 FROM {{ ref('staging_mapping') }}

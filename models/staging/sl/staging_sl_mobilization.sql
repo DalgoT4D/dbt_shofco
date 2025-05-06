e96c2897cdc16c696e41392e0380c6db2302f79b
Rodgers98 <companybells@gmail.com>
2025-05-06 10:23:25 +0300

    deleted_schema_on_my_staging

--- models/staging/sl/staging_sl_mobilization.sql
+++ models/staging/sl/staging_sl_mobilization.sql
@@ -2,7 +2,7 @@
     config(
         materialized='incremental',
         unique_key='case_id',
-        schema='staging_sl',
+        
         alias='sl_mobilization',
         tags=['commcare_extraction', 'sl_cases', 'sustainable_livelihoods', 'mobilization']
     )

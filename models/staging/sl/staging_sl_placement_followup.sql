0dcbe30764e59728290eee0664b8bc85a97737a5
Rodgers1998 <companybells@gmail.com>
2025-06-17 08:59:56 +0300

    added placement followup

--- models/staging/sl/staging_sl_placement_followup.sql
+++ models/staging/sl/staging_sl_placement_followup.sql
@@ -6,6 +6,8 @@
 WITH raw_data AS (
     SELECT
         data::jsonb AS form_data,
+
+        -- Metadata
         data::jsonb -> 'form' -> 'meta' ->> 'instanceID' AS form_id,
         data::jsonb -> 'form' -> 'meta' ->> 'username' AS enumerator_username,
         data::jsonb -> 'form' -> 'meta' ->> 'userID' AS enumerator_user_id,
@@ -16,9 +18,9 @@ WITH raw_data AS (
         data::jsonb -> 'form' -> 'case' ->> '@user_id' AS user_id,
         data::jsonb -> 'form' -> 'case' ->> '@date_modified' AS date_modified,
 
-        -- Participant info
+        -- Participant info from case.update
         data::jsonb -> 'form' -> 'case' -> 'update' ->> 'pp_fullname' AS participant_name,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'pp_sex' AS sex,
+        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'pp_unique_id' AS unique_id,
 
         CASE
             WHEN NULLIF(data::jsonb -> 'form' -> 'case' -> 'update' ->> 'pp_age', '') IS NOT NULL
@@ -27,31 +29,30 @@ WITH raw_data AS (
             ELSE NULL
         END AS age,
 
+        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'pp_sex' AS sex,
         data::jsonb -> 'form' -> 'case' -> 'update' ->> 'pp_shofco_county' AS county,
         data::jsonb -> 'form' -> 'case' -> 'update' ->> 'pp_shofco_subcounty' AS subcounty,
         data::jsonb -> 'form' -> 'case' -> 'update' ->> 'pp_ahofco_ward' AS ward,
 
-        -- Employment placement info
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'job_position_pl' AS job_position,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'income_on_average_pl' AS average_income,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'name_of_organization_pl' AS organization_name,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'organization_sector_pl' AS organization_sector,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'nature_of_emplyment_pl' AS nature_of_employment,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'placement_opportunity_pl' AS placement_opportunity,
+        -- Employment placement info (from nested form.business_transitions)
+        data::jsonb -> 'form' -> 'business_transitions' ->> 'type_of_business_pl' AS type_of_business,
+        data::jsonb -> 'form' -> 'business_transitions' ->> 'income_on_average_pl' AS average_income,
+        data::jsonb -> 'form' -> 'business_transitions' ->> 'documents_pl' AS documents,
+        data::jsonb -> 'form' -> 'business_transitions' ->> 'placement_opportunity_pl' AS placement_opportunity,
+        data::jsonb -> 'form' -> 'business_transitions' ->> 'county_of_job_transition_pl' AS county_of_transition,
+        data::jsonb -> 'form' -> 'business_transitions' ->> 'region_of_business_transition_pl' AS region_of_transition,
+        data::jsonb -> 'form' -> 'business_transitions' ->> 'participant_program_pl' AS participant_program,
 
-        -- Skills/training data
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'training_activity_pl' AS training_activity,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'skills_gained_pl' AS skills_gained,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'certifications_achieved_pl' AS certifications_achieved,
+        -- Training and skills (from form root)
+        data::jsonb -> 'form' ->> 'training_activity_pl' AS training_activity,
+        data::jsonb -> 'form' ->> 'certifications_achieved_pl' AS certifications_achieved,
+        data::jsonb -> 'form' ->> 'skills_gained_pl' AS skills_gained,
 
-        -- Transition metadata
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'region_of_business_transition_pl' AS region_of_transition,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'county_of_job_transition_pl' AS county_of_transition,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'participant_program_pl' AS participant_program,
-        data::jsonb -> 'form' -> 'case' -> 'update' ->> 'new_role_through_shofco_support_pl' AS new_role_through_shofco_support,
+        -- Staff and extras
+        data::jsonb -> 'form' ->> 'staff_name_pl' AS staff_name,
 
-        -- Extra metadata
-        data::jsonb -> 'metadata' ->> 'submit_ip' AS submit_ip
+        -- Submit IP (from root)
+        data::jsonb ->> 'submit_ip' AS submit_ip
 
     FROM {{ source('staging_sl', 'Placement_Followup') }}
     WHERE (data::jsonb ->> 'archived') IS NULL OR (data::jsonb ->> 'archived') = 'false'

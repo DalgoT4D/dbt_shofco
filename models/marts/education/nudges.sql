b7f2e93a3ec6d88987c136e1fd91d471dcd4d55f
Ritabrata Roy <royred88@gmail.com>
2025-07-23 21:07:26 +0530

    models updated for new charts viz
    education_scholarship_categories_agg:
    col-boarding_or_day
    >cleaned: trim, case diff, nulls
    col- county, subcounty
    >cleaned: county/subcounty- case diff, _ , -,trim, nulls
    
    nudges:
    col- county, subcounty
    >cleaned: county/subcounty- case diff, _ , -,trim, nulls
    
    public_partnerships:
    col- county, subcounty
    >cleaned: county/subcounty- case diff, _ , -,trim, nulls
    col-high_touch_low_touch
    >cleaned:trim
    col-students_enrolled
    >cleaned:cast to integer

--- models/marts/education/nudges.sql
+++ models/marts/education/nudges.sql
@@ -3,23 +3,38 @@
     tags=["education_expansion", "education"]
 ) }}
 
+WITH cleaned_staging_nudges AS (
 SELECT
     term,
     CONCAT('Grade ', grade) AS grade,
     cohort,
     year,
-    county,
-    subcounty,
+    initcap(trim(' ' from county)) as county,
+    translate(initcap(trim(' ' from subcounty)),'_,-', ' ') as subcounty,
+    gender,
+    nudge_type
+
+FROM {{ ref('staging_nudges') }}
+
+)
+
+SELECT
+    term,
+    grade,
+    cohort,
+    year,
+    COALESCE(NULLIF(county, ''), 'Unknown') AS county,
+    COALESCE(NULLIF(subcounty, ''), 'Unknown') AS subcounty,
     gender,
     nudge_type,
     COUNT(*) AS nudge_count
-FROM {{ ref('staging_nudges') }}
+FROM cleaned_staging_nudges
 GROUP BY
     term,
-    CONCAT('Grade ', grade),
+    grade,
     year,
-    county,
-    subcounty,
+    COALESCE(NULLIF(county, ''), 'Unknown'),
+    COALESCE(NULLIF(subcounty, ''), 'Unknown'),
     gender,
     nudge_type,
     cohort

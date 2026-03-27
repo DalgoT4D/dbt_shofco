58c87e8b20c25cbe6ce7d4c8b63c5a440627a453
Rodgers1998 <companybells@gmail.com>
2026-03-27 12:19:23 +0300

    added location to apprenticeship and vocational training

--- models/marts/sl/apprenticeship.sql
+++ models/marts/sl/apprenticeship.sql
@@ -5,10 +5,8 @@ select
     date_of_registration,
     pp_unique_id,
     pp_fullname,
-    'Apprenticeship' as service,
     gender,
     nationality,
-    refugee_type,
     kenyan_national_id_number_dir,
     county,
     subcounty,
@@ -16,13 +14,17 @@ select
     primary_phone_number,
     phone_last_8_digits,
     is_pwd,
-    type_of_disability_dir,
     is_young_mother,
     apprenticeship_provider_apr,
     skill_enrolled_apr,
     skill_name,
     sector_name,
-    placement_date_apr
+    placement_date_apr,
+    gps_of_business_location_apbl_raw,
+    apprenticeship_latitude,
+    apprenticeship_longitude,
+    apprenticeship_altitude,
+    apprenticeship_accuracy
 from {{ ref('staging_sl_case_table') }}
 where apprenticeship_provider_apr is not null
   and trim(apprenticeship_provider_apr) != ''
